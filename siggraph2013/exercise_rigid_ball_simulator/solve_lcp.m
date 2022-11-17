aef5702c99d250056ea80c9ec522d92499bd5017
raymonraymon <raymond03152410@163.com>
2022-11-17 18:02:16 +0800

    add lcp

--- siggraph2013/exercise_rigid_ball_simulator/solve_lcp.m
+++ siggraph2013/exercise_rigid_ball_simulator/solve_lcp.m
@@ -1,5 +1,19 @@
 function lambda = solve_lcp(A, b, lambda)
 
-% TODO add your own solver code here!!!
+LD = tril(A);
+U  = triu(A,1);
 
+for i=1:100
+    
+    lambda = -LD\(U*lambda + b);
+    lambda = max(0,lambda);
+    
+    w = (LD*(U*lambda)) + b;
+    H = min(w,lambda);
+    theta = H'*H;
+    
+    if theta < 0.0001
+        return
+    end
+end
 end
