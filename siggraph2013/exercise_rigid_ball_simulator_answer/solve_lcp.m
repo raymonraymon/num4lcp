972d99fc8d8e84a8802c68efec656cdc56ebbf00
raymonraymon <raymond03152410@163.com>
2022-12-02 10:09:17 +0800

    add an simple example

---
+++ siggraph2013/exercise_rigid_ball_simulator_answer/solve_lcp.m
@@ -0,0 +1,20 @@
+function lambda = solve_lcp(A, b, lambda)
+
+LD = tril(A);
+U  = triu(A,1);
+
+for i=1:100
+    
+    lambda = -LD\(U*lambda + b);
+    lambda = max(0,lambda);
+    
+    w = (LD*(U*lambda)) + b;
+    H = min(w,lambda);
+    theta = H'*H;
+    
+    if theta < 0.0001
+        return
+    end
+    
+end
+end
